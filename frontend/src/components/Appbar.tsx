ce9a629df312e2aace18f7634447a5f0513f95ba
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-03-15 22:40:45 +0530

    Avatar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,6 +1,7 @@
 import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
+
 export const Appbar = () => {
   return <div className="border-b flex justify-between px-10 py-4">
     <Link to={'/blogs'} className="flex flex-col font-bold justify-center cursor-pointer">
